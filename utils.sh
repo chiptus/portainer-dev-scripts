fbc85ead46fd34895b01ae096912f0314d684cde
Chaim Lev-Ari <chaim.lev-ari@portainer.io>
2026-04-27 13:03:09 +0300

    feat: create release branch

--- utils.sh
+++ utils.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-function get_dev_branch_name() {
-  origin_branch="$1"
-  # if the branch is already a dev branch, return 1
-  if [[ $origin_branch == *auto-develop* ]]; then
-    echo "this is already a dev branch"
-    return 1
-  fi
-
-  echo "$origin_branch" | awk '/EE-[0-9]+\/+/{sub(/EE-[0-9]+\//, "&auto-develop/")} 1'
-}
-
-function get_other_edition_path() {
-  if [[ $PWD == *portainer-ee ]]; then
-    echo "$HOME/portainer-org/portainer"
-  else
-    echo "$HOME/portainer-org/portainer-ee"
-  fi
-}
-
 function get_current_branch_name() {
   git branch --show-current
 }
 
-function gpb() {
-  git push -u origin "$(get_current_branch_name)"
+# Inserts "release-<version>/" after the ticket segment.
+# get_release_branch_name "fix/BE-12885/workflow-status" "BE-12885" "release/2.34"
+# -> "fix/BE-12885/release-2.34/workflow-status"
+function get_release_branch_name() {
+  local branch="$1" ticket="$2" release_base="$3"
+  local version
+  version=$(echo "$release_base" | sed 's|.*/||')
+  echo "$branch" | sed "s|${ticket}/|${ticket}/release-${version}/|"
+}
+
+# Inverse: strips "release-<version>/" from after the ticket segment.
+# get_dev_branch_name "fix/BE-12885/release-2.34/workflow-status" "BE-12885" "release/2.34"
+# -> "fix/BE-12885/workflow-status"
+function get_dev_branch_name() {
+  local branch="$1" ticket="$2" release_base="$3"
+  local version
+  version=$(echo "$release_base" | sed 's|.*/||')
+  echo "$branch" | sed "s|${ticket}/release-${version}/|${ticket}/|"
 }
